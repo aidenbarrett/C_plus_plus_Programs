1f3d065a9150d87ec13ebce4e1df7c52fe0fe35e
Aiden Barrett <Aidenbarrett101@gmail.com>
2017-11-22 22:36:22 +0000

    Updated

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-void printSomething(); // Function prototype / prototyping a function to let
-                       // program know function will be called at some stage.
+void printSomething(); // Function prototype / prototyping a function to let the
+                       // program know that the function will be called at some stage.
 int main()
 {
-    printSomething();
+    printSomething(); // Calls printSomething method.
     return 0;
 }
 
-void printSomething(){ // Function name is printSomething and void is return type
+void printSomething(){ // Function name is printSomething and void is return type.
     cout << "Hello!" << endl;
 }
 
