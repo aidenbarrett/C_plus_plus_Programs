e281cd211e6485e1cafc71fefa7f27c7b40ac58e
Aiden Barrett <Aidenbarrett101@gmail.com>
2017-11-23 19:44:25 +0000

    New changes

--- Functions_Multi_Parameters/main.cpp
+++ Functions_Multi_Parameters/main.cpp
@@ -2,15 +2,23 @@
 
 using namespace std;
 
-int addNumbers(int, int);
+int addNumbers(int, int, int, int); // function prototype
+int subtractNumbers(int, int, int, int); // function prototype
 
 int main()
 {
-    cout << addNumbers(20,120);
+    cout << "\n" << addNumbers(20,120,130,10) << endl;
+    cout << "\n" << subtractNumbers(560,60,400, 72) << endl;
     return 0;
 }
 
-int addNumbers(int x, int y){
-    int answer = x + y;
-    return answer;
+int addNumbers(int w, int x, int y, int z){ // inserts variables and returns with answer
+    int addition = w + x + y + z;
+    return addition;
 }
+
+int subtractNumbers(int a, int b, int c, int d){
+    int subtraction = a - b - c - d;
+    return subtraction;
+}
+
