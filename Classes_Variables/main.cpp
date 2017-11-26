652356c4e0134c5b128990c56bdc76a6dbf49e94
Aiden Barrett <Aidenbarrett101@gmail.com>
2017-11-26 00:10:21 +0000

    New changes

--- Classes_Variables/main.cpp
+++ Classes_Variables/main.cpp
@@ -3,9 +3,30 @@
 
 using namespace std;
 
+/* --------------------------------------------------------------------
+    EXAMPLE OF BAD DESIGN CHOICE - CLASS VARIABLES SHOULD NOT BE PUBLIC
+   --------------------------------------------------------------------
+
 class AidenClass{
-    public:             // Access Modifer (Public).
+    public:             // Access Modifer (Public). Can be accessed in main().
         string name;    // name is a variable of type String.
+};
+
+int main()
+{
+    AidenClass AidenObject;
+    AidenObject.name = "Hello World!";
+
+    cout << "\n" << AidenObject.name << endl;
+
+    return 0;
+}
+-------------------------------------------------------------------- */
+
+
+class AidenClass{
+    private:             // Access Modifer (Private).
+        string name;
 };
 
 int main()
